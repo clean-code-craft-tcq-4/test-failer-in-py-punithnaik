07f5d1588474cfc1615dc75000246a45aa20dd53
Naik Punith <pnu5kor@bosch.com>
2022-06-20 11:40:35 +0530

    Added assert

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,16 @@
 
 def print_color_map():
+    pair_num = []
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            pair_num.append(i * 5 + j)
+    return len(major_colors) * len(minor_colors),pair_num
 
 
-result = print_color_map()
+result,pair_num = print_color_map()
 assert(result == 25)
+assert(pair_num[0] == 1)
 print("All is well (maybe!)\n")
