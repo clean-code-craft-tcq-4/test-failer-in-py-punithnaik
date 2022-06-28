f4b9f46619e4c56f0ffc78dc139d8b9a944b809d
Naik Punith <pnu5kor@bosch.com>
2022-06-28 17:20:07 +0530

    Fixed the bug

--- alerter.py
+++ alerter.py
@@ -16,10 +16,11 @@ def alert_in_celcius(farenheit):
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 0
+        alert_failure_count += 1
 
 
 alert_in_celcius(400.5)
+assert(alert_failure_count == 1)
 alert_in_celcius(303.6)
 assert(alert_failure_count == 2)
 print(f'{alert_failure_count} alerts failed.')
--- misaligned.py
+++ misaligned.py
@@ -5,12 +5,12 @@ def print_color_map():
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-            pair_num.append(i * 5 + j)
+            print(f'{i * 5 + j+1} | {major} | {minor}')
+            pair_num.append(i * 5 + j+1)
     return len(major_colors) * len(minor_colors),pair_num
 
-
-result,pair_num = print_color_map()
+ 
+result, pair_num = print_color_map()
 assert(result == 25)
 assert(pair_num[0] == 1)
 print("All is well (maybe!)\n")
